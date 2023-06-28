3ba19775a7302c3335b4b7306c75512608265ead
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-06-27 21:09:58 -0300

    add styles

--- styles.js
+++ styles.js
@@ -1,13 +1,13 @@
-import { StyleSheet, StatusBar } from "react-native"
+import { StyleSheet, StatusBar } from 'react-native'
 
 export const styles = StyleSheet.create({
   safeArea: {
-    flex: 1
+    flex: 1,
   },
   container: {
     flex: 1,
     marginHorizontal: 20,
-    paddingTop: StatusBar.currentHeight,
+    paddingTop: StatusBar.currentHeight || 0,
   },
   inputContainer: {
     flexDirection: 'row',
@@ -19,5 +19,66 @@ export const styles = StyleSheet.create({
     paddingHorizontal: 10,
     flex: 0.95,
     color: '#212121',
-  }
+  },
+  listContainer: {
+    marginTop: 25,
+    flex: 1,
+  },
+  itemList: {
+    gap: 15,
+    paddingBottom: 20,
+  },
+  item: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    backgroundColor: '#D4D7ED',
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    color: '#212121',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3,
+  },
+  itemText: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: '#212121',
+  },
+  modalContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 20,
+    flex: 1,
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBotton: 10,
+  },
+  modalDetailContainer: {
+    paddingVertical: 20,
+  },
+  modalDetailMessage: {
+    fontSize: 14,
+    color: '#212121',
+  },
+  selectedTask: {
+    fontSize: 14,
+    color: '#212121',
+    fontWeight: 'bold',
+    paddingVertical: 10,
+    textAlign: 'center',
+  },
+  modalButtonContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-around',
+    marginHorizontal: 20,
+  },
 })
